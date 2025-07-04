334727ec5fa554ed21b8e3b94412fbbcb51c38dd
RM <134272733+rayman1337@users.noreply.github.com>
2025-07-04 15:51:17 +0000

    added notes view - edit.php

---
+++ app/views/notes/edit.php
@@ -0,0 +1,11 @@
+<?php require_once '../app/views/templates/header.php'; ?>
+
+<h2>Edit Reminder</h2>
+<form method="POST" action="">
+    <label>Subject:</label><br>
+    <input type="text" name="subject" value="<?= htmlspecialchars($data['note']['subject']) ?>" required><br><br>
+    <label><input type="checkbox" name="completed" <?= $data['note']['completed'] ? 'checked' : '' ?>> Completed</label><br><br>
+    <button type="submit">Update</button>
+</form>
+
+<?php require_once '../app/views/templates/footer.php'; ?>
